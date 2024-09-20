a0568173229b4a2ed9d66aead5df41928418215f
Patrick Noah Policarpio <patrickpolicarpio08@gmail.com>
2024-09-20 14:16:34 +0800

    init auth api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,56 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
+import * as compression from 'compression';
+
+import { GlobalExceptionFilter } from '@/filters';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  /**
+   * Global configuration
+   */
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  });
+  app.use(helmet({ contentSecurityPolicy: true }));
+  app.use(compression());
+
+  /**
+   * Global filters
+   */
+  app.useGlobalFilters(new GlobalExceptionFilter());
+
+  /**
+   * Swagger config
+   */
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('Automatically generated API documentation')
+    .setVersion('1.0')
+    .setExternalDoc(
+      'Export API documentation as json file',
+      'docs/api/download',
+    )
+    .addTag('Auth API', 'Auth account management')
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('docs/api', app, swaggerDocument, {
+    customSiteTitle: 'CUL Transport API Swagger Documentation',
+  });
+
+  /**
+   * Run the application
+   */
+  await app.listen(process.env.APP_PORT);
 }
 bootstrap();
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Body, Post, HttpStatus } from '@nestjs/common';
+import { ApiTags, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('Auth API')
 @Controller({
   path: 'auth',
   version: '1',
 })
 export class AuthController {
+  @ApiResponse({
+    status: 200,
+    description: 'Authenticate user credentials success',
+  })
   @Post('/login')
-  async loginHandler(@Body() paylaod: any) {
+  async signinHandler(@Body() paylaod: any) {
     return null;
   }
 }
